3bd4fc7d040e1ab294d627c2a25fd0ffe88ce391
Thea Blachon <thea.blachon73@gmail.com>
2024-07-08 11:53:50 +0200

    PATCH ESLINT

---
+++ .eslintrc.mjs
@@ -0,0 +1,13 @@
+export default {
+    env: {
+        es2021: true,
+        node: true
+    },
+    parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module'
+    },
+    rules: {
+        // Ajoutez vos règles personnalisées ici
+    }
+};
